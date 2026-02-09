a24dc8e4e01dc3856128b87fe3e3a9e710ea0a90
Zorko <165199108+NejcZorko007@users.noreply.github.com>
2026-02-09 07:55:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Copyright 2026 Your Name
+#Copyright 2026 Zorko Industries ©
 
 #Licensed under the Apache License, Version 2.0 (the "License");
 #you may not use this file except in compliance with the License.
